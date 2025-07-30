8eaf9bf447199294952cfe7a38ddb2c257fc197c
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2025-07-31 00:25:36 +0300

    page of mess is done

--- src/components/dashboard/dmess/dmess.jsx
+++ src/components/dashboard/dmess/dmess.jsx
@@ -25,10 +25,74 @@ const Dmess =()=>{
                     <div className="cards">
                             <div className="card">
                                 <div className="card-num">
-                                    <span class="number">22</span>
+                                    <span class="number"><img src="./delete.png" alt="" /></span>
                                 </div>
                                 <div className="card-info">
-                                    
+                                    <div className="main-info">
+                                        <h3>11:20</h3>
+                                        <h3>صالح عبد الرحمن</h3>
+                                    </div>
+                                    <div className="contact-info">
+                                            <p>something@gmail.com</p>
+                                            <p>+90 552-593-90-69</p>
+                                    </div>
+                                    <div className="mess">
+                                        <p>نعمل شراكة شرايكم</p>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div className="card">
+                                <div className="card-num">
+                                    <span class="number"><img src="./delete.png" alt="" /></span>
+                                </div>
+                                <div className="card-info">
+                                    <div className="main-info">
+                                        <h3>11:20</h3>
+                                        <h3>صالح عبد الرحمن</h3>
+                                    </div>
+                                    <div className="contact-info">
+                                            <p>something@gmail.com</p>
+                                            <p>+90 552-593-90-69</p>
+                                    </div>
+                                    <div className="mess">
+                                        <p>نعمل شراكة شرايكم</p>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div className="card">
+                                <div className="card-num">
+                                    <span class="number"><img src="./delete.png" alt="" /></span>
+                                </div>
+                                <div className="card-info">
+                                    <div className="main-info">
+                                        <h3>11:20</h3>
+                                        <h3>صالح عبد الرحمن</h3>
+                                    </div>
+                                    <div className="contact-info">
+                                            <p>something@gmail.com</p>
+                                            <p>+90 552-593-90-69</p>
+                                    </div>
+                                    <div className="mess">
+                                        <p>نعمل شراكة شرايكم</p>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div className="card">
+                                <div className="card-num">
+                                    <span class="number"><img src="./delete.png" alt="" /></span>
+                                </div>
+                                <div className="card-info">
+                                    <div className="main-info">
+                                        <h3>11:20</h3>
+                                        <h3>صالح عبد الرحمن</h3>
+                                    </div>
+                                    <div className="contact-info">
+                                            <p>something@gmail.com</p>
+                                            <p>+90 552-593-90-69</p>
+                                    </div>
+                                    <div className="mess">
+                                        <p>نعمل شراكة شرايكم</p>
+                                    </div>
                                 </div>
                             </div> 
                     </div>
