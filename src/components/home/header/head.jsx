bbc1a765ad366d080e906683e62bd683f6c7e935
ahmedalaa2544 <ahmedalaa2544@gmail.com>
2025-08-20 04:06:58 +0300

    fix menu responsive

--- src/components/home/header/head.jsx
+++ src/components/home/header/head.jsx
@@ -1,27 +1,43 @@
-import './hStyle.css';
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from '../../../userSlice';
-import { StartBtn } from '../../startBtn';
+import "./hStyle.css"
+import {
+  NavLink,
+  useLocation,
+  useNavigate,
+} from "react-router-dom"
+import {
+  useSelector,
+  useDispatch,
+} from "react-redux"
+import { logoutUser } from "../../../userSlice"
+import { StartBtn } from "../../startBtn"
+import { useEffect } from "react"
 
 const Header = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const location = useLocation()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const user = useSelector((state) => state.users.user);
+  const user = useSelector(
+    (state) => state.users.user
+  )
 
   const menuItems = [
-    { path: '/', label: 'الرئيسية' },
-    { path: '/contact', label: 'تواصل' },
-    { path: '/games', label: 'إلعب' },
-    { path: '/packages', label: 'الباقات' },
-  ];
+    { path: "/", label: "الرئيسية" },
+    {
+      path: "/contact",
+      label: "تواصل",
+    },
+    { path: "/games", label: "إلعب" },
+    {
+      path: "/packages",
+      label: "الباقات",
+    },
+  ]
 
   const handleLogout = () => {
-    dispatch(logoutUser());
-    navigate("/login");
-  };
+    dispatch(logoutUser())
+    navigate("/login")
+  }
 
   return (
     <header>
@@ -34,35 +50,73 @@ const Header = () => {
                 <span></span>
                 <span></span>
               </span>
-              <ul>
-                {menuItems.map((item) => (
-                  <li
-                    key={item.path}
-                    className={location.pathname === item.path ? 'active' : ''}
-                  >
-                    <NavLink to={item.path}>
-                      {item.label}
-                      {location.pathname === item.path && <img src="./dashr.png" alt="" />}
-                    </NavLink>
-                  </li>
-                ))}
+              <ul className="open">
+                {menuItems.map(
+                  (item) => (
+                    <li
+                      key={item.path}
+                      className={
+                        location.pathname ===
+                        item.path
+                          ? "active"
+                          : ""
+                      }
+                    >
+                      <NavLink
+                        to={item.path}
+                      >
+                        {item.label}
+                        {location.pathname ===
+                          item.path && (
+                          <img
+                            src="./dashr.png"
+                            alt=""
+                          />
+                        )}
+                      </NavLink>
+                    </li>
+                  )
+                )}
 
                 {user && (
                   <>
                     {/* رابط صفحة حسابي */}
                     <li
-                      className={location.pathname === "/user" ? 'active' : ''}
+                      className={
+                        location.pathname ===
+                        "/user"
+                          ? "active"
+                          : ""
+                      }
                     >
                       <NavLink to="/user">
                         حسابي
-                        {location.pathname === "/user" && <img src="./dashr.png" alt="" />}
+                        {location.pathname ===
+                          "/user" && (
+                          <img
+                            src="./dashr.png"
+                            alt=""
+                          />
+                        )}
                       </NavLink>
                     </li>
 
                     {/* زر تسجيل الخروج */}
                     <li className="exit">
-                      <button onClick={handleLogout} className="logout-btn">
-                        تسجيل الخروج <img src="./exit.png" alt="" style={{ width: 20 }} />
+                      <button
+                        onClick={
+                          handleLogout
+                        }
+                        className="logout-btn"
+                      >
+                        تسجيل الخروج{" "}
+                        <img
+                          src="./exit.png"
+                          alt=""
+                          style={{
+                            width: 20,
+                          }}
+                        />
                       </button>
                     </li>
                   </>
@@ -73,14 +127,22 @@ const Header = () => {
             {user ? (
               <>
                 <button className="points">
-                  <img src="./ydot.png" alt="" style={{ width: 25 }} /> {user.coins}
+                  <img
+                    src="./ydot.png"
+                    alt=""
+                    style={{
+                      width: 25,
+                    }}
+                  />{" "}
+                  {user.coins}
                 </button>
                 <StartBtn />
-                
               </>
             ) : (
               <button
-                onClick={() => navigate("/login")}
+                onClick={() =>
+                  navigate("/login")
+                }
                 className="play"
               >
                 تسجيل
@@ -88,11 +150,15 @@ const Header = () => {
             )}
           </div>
           <a href="/">
-            <img src="./logo.png" alt="logo" style={{ width: 100 }} />
+            <img
+              src="./logo.png"
+              alt="logo"
+              style={{ width: 100 }}
+            />
           </a>
         </div>
       </div>
     </header>
-  );
-};
-export default Header;
+  )
+}
+export default Header
