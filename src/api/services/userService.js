902d9eb8eb651c89179f78d914d62d9f691b6603
ahmedalaa2544 <ahmedalaa2544@gmail.com>
2025-08-26 04:11:23 +0300

    add payment

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -4,27 +4,26 @@ export const passUser = async (oldPassword, newPassword) => {
   try {
     const authData = localStorage.getItem("authData");
     let token = null;
-    
+
     if (authData) {
       try {
         const parsedAuthData = JSON.parse(authData);
         if (parsedAuthData.user) {
           const rawToken = localStorage.getItem("token");
           if (rawToken) {
-            token = rawToken.startsWith('"') && rawToken.endsWith('"') 
-              ? rawToken.slice(1, -1) 
-              : rawToken;
+            token =
+              rawToken.startsWith('"') && rawToken.endsWith('"') ? rawToken.slice(1, -1) : rawToken;
           }
         }
       } catch (parseError) {
         console.error("Error parsing authData:", parseError);
       }
     }
-    
+
     if (!token) {
       throw new Error("لم يتم العثور على التوكن، يرجى تسجيل الدخول مرة أخرى");
     }
-    
+
     const response = await api.post(
       "/user/change-password",
       { oldPassword, newPassword },
@@ -37,5 +36,27 @@ export const passUser = async (oldPassword, newPassword) => {
     return response.data;
   } catch (error) {
     throw error.response?.data?.message || error.message || "خطأ فى تغير كلمة المرور";
+  }
+};
+export const createPayment = async (price) => {
+  const rawToken = localStorage.getItem("token");
+  let token;
+  if (rawToken) {
+    token = rawToken.startsWith('"') && rawToken.endsWith('"') ? rawToken.slice(1, -1) : rawToken;
+  } else return;
+  try {
+    const response = await api.post(
+      "/user/create-payment",
+      { price },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log("response", response.data);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || error.message || "خطأ فى انشاء الدفع";
   }
 };
