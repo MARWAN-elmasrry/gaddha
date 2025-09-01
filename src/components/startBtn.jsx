3c128b828c8976137c8f37dd78c7a56100a80ac1
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2025-09-01 12:38:29 +0300

    add some animation on btn play

--- src/components/startBtn.jsx
+++ src/components/startBtn.jsx
@@ -1,6 +1,9 @@
+import "./Startbtn.css";
+
 export const StartBtn = () => {
   return (
     <>
+    <div className="btn-play">
       <button
         className="play"
         style={{ position: "relative", zIndex: "100" }}
@@ -11,6 +14,11 @@ export const StartBtn = () => {
       >
         العب
       </button>
+        <div className="an an1"></div>
+        <div className="an an2"></div>
+        <div className="an an3"></div>
+        <div className="an an4"></div>
+      </div>
     </>
   );
 };
