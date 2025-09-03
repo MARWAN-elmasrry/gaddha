46e7629fac73dc0664124baf2234ab4b6dc6fde0
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2025-09-03 15:23:13 +0300

    add loading

--- src/components/home/cate/category.jsx
+++ src/components/home/cate/category.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import "./cStyle.css";
 import { LatestThreeCate } from "../../../api/services/userService";
+import { Loading } from "../../dashboard/dmain/dmain";
 
 const Category = () => {
   const [lastThree, setLastThree] = useState([]);
+  const [loadingLast , setLoadingLast] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,6 +21,9 @@ const Category = () => {
             "خطأ فى جلب المجموعات"
         );
       }
+      finally{
+        setLoadingLast(false);
+      }
     };
 
     fetchData();
@@ -32,7 +37,10 @@ const Category = () => {
             <h1>الفئات</h1>
             <h2>هنا تشوفون جزء من فئاتنا, سووا حساب وشوفوا باقي الفئات</h2>
           </div>
-          <div className="cards">
+          {loadingLast?(<>
+            <Loading />
+          </>):(<>
+            <div className="cards">
             {lastThree.length > 0 ? (
               lastThree.map((cat, index) => (
                 <div key={index} className="card">
@@ -51,7 +59,7 @@ const Category = () => {
               <h1>لا يوجد فئات حالياً</h1>
             )}
           </div>
-
+          </>)}
           <div className="bar-b">
             <button className="play"
             onClick={(e) => {
